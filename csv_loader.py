10db8f46f9fab1bfe664a48f933d8e743286a14f
Anthony <tonyudeagbala@gmail.com>
2023-01-03 13:40:03 +0100

    feat: load sales data set

--- csv_loader.py
+++ csv_loader.py
@@ -18,7 +18,7 @@ def format_date(date_string):
         to python date format
     """
 
-    full_month_format = "%d %B %Y"
+    full_month_format = "%d/%m/%Y"
 
     return datetime.strptime(date_string, full_month_format)
 
@@ -57,7 +57,40 @@ def load_stores_data_set():
 
 
 def load_sales_data_set():
+    """
+        A function that loads sales
+        data set into mysql database
+    """
+
     path = 'datasets/sales_data-set.csv'
+
+    try:
+        #   Open and read csv file
+        with open(path) as f:
+            reader = csv.reader(f)
+            for row in reader:
+                #   create db connection
+                cursor = con.cursor()
+                #   skip csv headers
+                if row[0] == 'Store':
+                    continue
+                else:
+                    #   formate date
+                    sales_date = format_date(row[2])
+                    #   convert is_holiday value from boolean string to tinyint for mysql field
+                    is_holiday = 0
+                    if row[4] != 'FALSE':
+                        is_holiday = 1
+                    
+                    #   insert record into db
+                    cursor.execute("INSERT into sales(store, department, date, weekly_sales, is_holiday) VALUES(%s, %s, %s, %s, %s)", (row[0], int(row[1]), sales_date, float(row[3]), is_holiday))
+                    con.commit()
+                    cursor.close() 
+        print("sales data set loaded successfully")
+    except Exception as e:
+        print("There was an issue loading the sales data set")
+        print(e)
+
 
 def load_store_info():
     path = 'datasets/store_info.csv'
@@ -65,4 +98,6 @@ def load_store_info():
 def load_features_data_set():
     path = 'datasets/Features_data_set.csv'
 
-load_stores_data_set()
+# load_stores_data_set()
+
+load_sales_data_set()
