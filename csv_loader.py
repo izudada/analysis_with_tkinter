9b493136d696f3f07b0af1e54de1766a18f4eaab
Anthony <tonyudeagbala@gmail.com>
2023-01-03 11:36:23 +0100

    feat: load stores data set

--- csv_loader.py
+++ csv_loader.py
@@ -1,7 +1,16 @@
 import csv
 import  os
 from datetime import datetime
+import mysql.connector as mysql
 
+
+#   database connection
+con = mysql.connect(
+        host="localhost", 
+        user="root", 
+        password="",
+        database="walmart"
+    )
 
 def format_date(date_string):
     """
@@ -15,13 +24,45 @@ def format_date(date_string):
 
 
 def load_stores_data_set():
-    pass
+    """
+        A function to load stores data set 
+        into a mysql database
+    """
 
-def load_store_info():
-    pass
+    #   csv path
+    path = 'datasets/stores_data-set.csv'
+    try:
+        #   Open and read csv file
+        with open(path) as f:
+            reader = csv.reader(f)
+            for row in reader:
+                #   create db connection
+                cursor = con.cursor()
+                #   skip csv headers
+                if row[0] == 'Store':
+                    continue
+                else:
+                    #   insert into store table
+                    size = 0
+                    if row[2] == '':
+                        cursor.execute("INSERT into store(id, store_type) VALUES(%s, %s)", (row[0] , row[1]))
+                    else:
+                        cursor.execute("INSERT into store(id, store_type, size) VALUES(%s, %s, %s)", (row[0] , row[1], int(row[2])))
+                    con.commit()
+                    cursor.close() 
+        print("stores data set loaded successfully")
+    except Exception as e:
+        print("There was an issue loading the stores data set")
+        print(e)
+
 
 def load_sales_data_set():
-    pass
+    path = 'datasets/sales_data-set.csv'
 
-def load_Features_data_set():
-    pass
+def load_store_info():
+    path = 'datasets/store_info.csv'
+
+def load_features_data_set():
+    path = 'datasets/Features_data_set.csv'
+
+load_stores_data_set()
